005f0f1ee84eeb21130451eb5253a83e6b83af6a
Alexander van Eck <alex@x-all.nl>
2017-08-27 10:30:47 +0200

    Fixed an issue where the line numbers of one file would bleed into the second file, therefore eventually producing errors on every line

--- flake8_class_newline/__init__.py
+++ flake8_class_newline/__init__.py
@@ -1,12 +1,16 @@
-new_lines_on = []
+new_lines_on = {}
 
 
-def new_line_checker(physical_line, line_number):
+def new_line_checker(physical_line, line_number, filename):
+    if filename not in new_lines_on:
+        new_lines_on[filename] = []
+
     if physical_line.startswith('class'):
-        new_lines_on.append(line_number + 1)
+        new_lines_on[filename].append(line_number + 1)
 
-    if line_number in new_lines_on:
-        if not physical_line.isspace():
+    if line_number in new_lines_on[filename]:
+        # if the line is not only whitespace and not empty
+        if not physical_line.isspace() and physical_line:
             # this means there's something on the line
             return 0, 'CNL100: Class definition does not have a new line.'
 
--- tests/test_checks.py
+++ tests/test_checks.py
@@ -3,8 +3,20 @@ from unittest import TestCase
 
 
 class TestChecks(TestCase):
-    def test_checks_class_new_lines(self):
+    def check_file(self, lines, filename):
         errors = []
+        for line_number, line in enumerate(lines):
+            errors.append(
+                new_line_checker(
+                    physical_line=line,
+                    line_number=line_number + 1,
+                    filename=filename
+                )
+            )
+
+        return errors
+
+    def test_checks_class_new_lines(self):
         expected_errors = [
             None,
             (0, 'CNL100: Class definition does not have a new line.'),
@@ -15,7 +27,7 @@ class TestChecks(TestCase):
             None
         ]
 
-        lines = [
+        a_file = [
             'class ClassWithoutANewLine(object):',
             'aaasome_arg = "a_string"',
             '',
@@ -24,12 +36,16 @@ class TestChecks(TestCase):
             '   ',
             'some_arg = "a_string"'
         ]
+        # This should produce no error.
+        # the error line of file 1 should not be propagated to file 2.
+        another_file = [
+            'class ClassWithoutANewLine(object):',
+            '',
+            'aaasome_arg = "a_string"',
+        ]
 
-        for line_number, line in enumerate(lines):
-            errors.append(
-                new_line_checker(
-                    physical_line=line, line_number=line_number
-                )
-            )
+        a_file_errors = self.check_file(a_file, 'a_file')
+        another_file_errors = self.check_file(another_file, 'another_file')
 
-        self.assertEqual(expected_errors, errors)
+        self.assertEqual(expected_errors, a_file_errors)
+        self.assertEqual([None, None, None], another_file_errors)
